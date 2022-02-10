269a0372374ae3ab18b618b47fd91741d515e62a
Tarun M <tarunsetty2002@gmail.com>
2022-02-10 21:04:15 +0530

    CORS 1

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const {Server} = require('socket.io')
 const app = express()
 
 app.use(cors())
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
 
 const server = http.createServer(app)
 
