2bbd9a615579b20bdf4f25da0058d294a0338d33
Tarun M <tarunsetty2002@gmail.com>
2022-02-13 23:23:49 +0530

    .

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const http = require('http')
 const cors = require('cors')
+const {Server} = require('socket.io')
 
 const app = express()
-const socketIO = require('socket.io')
-const server = http.createServer(app)
-// const io = socketIO(server);
 
 app.use(cors())
 app.use((req, res, next) => {
@@ -17,9 +15,7 @@ app.use((req, res, next) => {
     next();
   });
 
-app.get("/", (req, res) => {
-res.send("server");
-});
+const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
@@ -28,16 +24,48 @@ const io = new Server(server, {
     }
 })
 
+
+let rooms = new Map();
+
 io.on("connection", socket => {
     console.log(`Connected: ${socket.id}`)
 
     socket.on("join-room", data =>{
-        socket.join(data.roomId)
-        console.log(`${data.user} joined room ${data.roomId}`)
+
+        let isJoined = false
+        if (data.isTeacher === true){
+            socket.join(data.roomId)
+            rooms.set(data.roomId, {teacher: data.user, memberCount: 1});
+            console.log(`${data.user} joined room ${data.roomId} ${data.isTeacher}`)
+            isJoined = true;
+        }else{
+            console.log(rooms.keys())
+    
+            if (rooms.has(data.roomId)){
+                console.log(`${data.user} joined room ${data.roomId} ${data.isTeacher} ${rooms.get(data.roomId).memberCount}`)
+                socket.join(data.roomId)
+                rooms.set(data.roomId, {...rooms.get(data.roomId), memberCount: rooms.get(data.roomId).memberCount + 1})
+                isJoined = true;
+            }
+        }
+        console.log(isJoined)
+        socket.emit("join-result", {isJoined: isJoined, teacher: rooms.get(data.roomId).teacher})
+    })
+
+
+    socket.on("leave-room", data => {
+        socket.leave(data.roomId)
+        console.log('left')
+        rooms.set({...rooms.get(data.roomId), memberCount: rooms.get(data.roomId).memberCount - 1})
+        if (rooms.get(data.roomId).memberCount == 0){
+            rooms.delete(data.roomId)
+            console.log(`${data.roomId} was deleted`)
+        }
+
+        socket.emit("leave-result", true);
     })
 
     socket.on("send-canvas", data => {
-        console.log(data)
         socket.to(data.roomId).emit("recieve-canvas", data.image)
     })
 
@@ -46,8 +74,6 @@ io.on("connection", socket => {
     })
 })
 
-const PORT = process.env.PORT || 5000;
-
-server.listen(PORT, () => {
+server.listen(5000, () => {
     console.log("SERVER RUNNING")
 })
