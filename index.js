db15153ce305ab131e0a580a2a370766a2bc27d6
Tarun M <tarunsetty2002@gmail.com>
2022-02-13 23:25:58 +0530

    .

--- index.js
+++ index.js
@@ -74,6 +74,8 @@ io.on("connection", socket => {
     })
 })
 
-server.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
     console.log("SERVER RUNNING")
 })
